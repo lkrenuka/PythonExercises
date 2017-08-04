eb4622f1fa95c923d2a41a0546d8d92472f40501
lkrenuka@gmail.com <lkrenuka@gmail.com>
2017-08-04 21:03:38 +0530

    First commit

---
+++ FindRepeatingLetterWord.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  4 19:49:22 2017
+
+@author: user
+"""
+
+def find_word_repeating_char(str):
+    str = str.lower()
+    new_str = []
+    for word in (str.split()):
+#        print (word)
+        c = word[0]
+        let=1
+        while let < len(word):
+            if c==word[let]:
+                new_str.append(word)
+            let += 1
+    return new_str
+
+str = "My name is Adity, Pop, Lol, Bob, Baby"
+new_str = find_word_repeating_char(str)
+print (new_str)
---
+++ README.md
@@ -0,0 +1,1 @@
+"Python Exercises - Logistic Regression from scratch, finding repeating characters in word of a sentence, etc" 
