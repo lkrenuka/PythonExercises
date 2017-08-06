c3c1624d705dc2d8904f462142c2226b19d23307
lkrenuka@gmail.com <lkrenuka@gmail.com>
2017-08-06 18:07:32 +0530

    Program to demonstrate property decorator

---
+++ PropertyDecorator.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug  6 17:50:11 2017
+
+@author: Renuka L K
+Program demonstrating @property decorator
+"""
+class Celcius:
+    def __init__(self, temperature=0):
+        self.temperature = temperature
+    
+    def to_fahrenheit(self):
+        self.temperature = (self.temperature * 1.8) + 37
+        return self.temperature
+    
+    def get_temperature(self):
+        print ("inside get")
+        return self._temperature
+    
+    def set_temperature(self, value):
+        print ("inside set")
+        if (value<-273):
+            raise ValueError ("Temperature below -273 is not valid")
+        self._temperature = value
+        
+    temperature = property(get_temperature, set_temperature)
+
